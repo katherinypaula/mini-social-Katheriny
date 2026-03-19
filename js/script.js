d1a93133e13227bab49b566402d8d97ea09895e1
katherinypaula <katheriny.4635045@aluno.mg.gov.br>
2026-03-19 16:40:41 +0000

    Update JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 let likeCount = 0;
+let curtido = false; // flag booleana
 
-function curtir() {
-  likeCount++;
+function curtir() { 
+
+ if(curtido == false){
+   likeCount++;
+   curtido = true; 
   document.getElementById("likeCount").innerText = likeCount;
-}
+ }else{
+   likeCount--;
+   curtido = true; 
+  document.getElementById("likeCount").innerText = likeCount;
+ }
+  
+   
+ }
 
-document.getElementById("likeBtn").addEventListener("click", curtir);
+ document.getElementById("likeBtn").addEventListener("click", curtir);
