18fe8c6221c2a413159778064064809109a6c6ad
katherinypaula <katheriny.4635045@aluno.mg.gov.br>
2026-04-28 18:25:57 +0000

    curtir

--- js/script.js
+++ js/script.js
@@ -1,36 +1,69 @@
+// === ESTADO (dados da aplicação) === 
 let likeCount = 0;
-let desCount = 0;
-
-let curtido = false; // flag booleana
+let dislikeCount = 0;
+let curtido = false;
 let descurtido = false;
 
+// === SERVICE (regras de negócio)
 function curtir() {
-
- if(curtido == false){
+  if (curtido == false){
     likeCount++;
     curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
- }else{
+    
+
+    if(descurtido == true){
+      dislikeCount--;
+      descurtido = false;
+    }
+
+  }else{
     likeCount--;
     curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
- }
+  }
+
 }
 
-
-
- function deslike () {
-if(descurtido == false){
-    desCount++;
+function descurtir() {
+  if(descurtido == false){
+    dislikeCount++;
     descurtido = true;
-    document.getElementById("desCount").innerText = desCount;
- }else{
-    desCount--;
+    
+
+    if(curtido == true){
+      likeCount--;
+      curtido = false;
+    }
+
+  }
+  else{
+    dislikeCount--;
     descurtido = false;
-    document.getElementById("desCount").innerText = desCount;
- }
- 
+  }
 }
 
-document.getElementById("likeBtn").addEventListener("click", curtir);
-document.getElementById("desBtn").addEventListener("click", deslike);
+//=== VIEWS (interface)===
+
+function atualizarTela(){
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
+}
+
+//=== CONTROLLER (intermediação evento/regras de negócio)
+
+function clicarCurtir(){
+  curtir();
+  atualizarTela();
+}
+function clicarDescurtir(){
+  descurtir();
+  atualizarTela();
+}
+
+
+// === EVENTOS ====
+document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
+document.getElementById("dislikeBtn").addEventListener("click", clicarDescurtir);
+
+// === INICIALIZAÇÃO DE TELA ===
+
+atualizarTela();
